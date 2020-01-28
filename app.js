530bf281b605b7a0ea6c5266f47c7eff8f7ae482
Sabrina Jackson <sabrina@wolfchild.com>
2020-01-27 18:06:50 -0800

    Assignment_Simple_To-Do_DOM_Scripting_Events_Part_1: Completed

--- app.js
+++ app.js
@@ -28,6 +28,35 @@ function onReady() {
     // empty the input
     newToDoText.value = ''; 
 
+    var spanDelete = document.createElement("span");
+    spanDelete.setAttribute("class", "delete");
+    spanDelete.innerHTML = "&nbsp;&#10007;&nbsp;";
+
+    spanDelete.onclick = deleteItem;
+    newLi.appendChild(spanDelete);
+
+    function deleteItem(e) {
+        var span = e.target;
+        var id= span.parentElement.id;
+        console.log("delete an item: " + id);
+        var key = "toDoList" + id;
+    localStorage.removeItem(key);
+
+    // find and remove the item in the array
+    for (var i = 0; i < toDoList.length; i++) {
+        if (toDoList[i].id == id) {
+            toDoList.splice(i, 1);
+            break;
+        }
+    }
+
+    // find and remove the item in the page
+    var li = e.target.parentElement;
+    var ul = document.getElementById("toDoList");
+    ul.removeChild(li);
+}             
+   
+
     });
 }
 
