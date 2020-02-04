679300e19ca52cfeefa43c1f735d487b9f19f9a9
Sabrina Jackson <sabrina@wolfchild.com>
2020-02-04 14:25:20 -0800

    Checkpoint_Simple_To-Do_DOM_Scripting_Events_Part_2: Completed

--- app.js
+++ app.js
@@ -1,65 +1,46 @@
 function onReady() {
+    const toDos = [];
     const addToDoForm = document.getElementById('addToDoForm');
-    const newToDoText = document.getElementById('newToDoText');
-    const toDoList = document.getElementById('toDoList');
-    addToDoForm.addEventListener('submit', () => {
-        event.preventDefault();
-    // get the text
-    let title = newToDoText.value;
+ 
+    function createNewToDo() {
+        const newToDoText = document.getElementById('newToDoText');
+        if (!newToDoText.value) { return; }
+        
+        toDos.push({
+            title: newToDoText.value,
+            complete: false
+        });
 
-    // create a new li 
-    let newLi = document.createElement('li');
-  
-    // create new input
-    let checkbox = document.createElement('input');
+    newToDoText.value = '';
+    renderTheUI();
+  }
 
-    // set the input's type to checkbox
-    checkbox.type = "checkbox";
+  function renderTheUI() {
+   const toDoList = document.getElementById('toDoList');
+      
+   toDoList.textContent = '';
 
-    // set the title
-    newLi.textContent = title;
-
-    // attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    // attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    // empty the input
-    newToDoText.value = ''; 
-
-    var spanDelete = document.createElement("span");
-    spanDelete.setAttribute("class", "delete");
-    spanDelete.innerHTML = "&nbsp;&#10007;&nbsp;";
-
-    spanDelete.onclick = deleteItem;
-    newLi.appendChild(spanDelete);
-
-    function deleteItem(e) {
-        var span = e.target;
-        var id= span.parentElement.id;
-        console.log("delete an item: " + id);
-        var key = "toDoList" + id;
-    localStorage.removeItem(key);
-
-    // find and remove the item in the array
-    for (var i = 0; i < toDoList.length; i++) {
-        if (toDoList[i].id == id) {
-            toDoList.splice(i, 1);
-            break;
-        }
-    }
-
-    // find and remove the item in the page
-    var li = e.target.parentElement;
-    var ul = document.getElementById("toDoList");
-    ul.removeChild(li);
-}             
+   toDos.forEach(function(toDo) {
+     const newLi = document.createElement('li');
+     const checkbox = document.createElement('input');
+     checkbox.type = "checkbox";
    
+     newLi.textContent = toDo.title;
 
-    });
+     toDoList.appendChild(newLi);
+     newLi.appendChild(checkbox);
+  });
 }
 
+      addToDoForm.addEventListener('submit', event => {
+      event.preventDefault();
+      createNewToDo();
+    });
+
+    renderTheUI();
+}  
+
 window.onload = function() {
-    onReady();
- };
+  onReady();
+};
+   
