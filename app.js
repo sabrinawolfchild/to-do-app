69e14dd3bcf2279020f109118f9f66a88eadf019
Sabrina Jackson <sabrina@wolfchild.com>
2020-02-05 15:49:20 -0800

    Assignment_Simple_To-Do_DOM_Scripting_Events_Part_2: Completed

--- app.js
+++ app.js
@@ -1,36 +1,50 @@
 function onReady() {
-    const toDos = [];
+    let toDos = [];
     const addToDoForm = document.getElementById('addToDoForm');
- 
+    let id = 0;
+    
     function createNewToDo() {
         const newToDoText = document.getElementById('newToDoText');
-        if (!newToDoText.value) { return; }
-        
+        if (!newToDoText.value){ return; }
+  
         toDos.push({
-            title: newToDoText.value,
-            complete: false
+          title: newToDoText.value,
+          complete: false,
+          id: id
         });
+        id++;
+        newToDoText.value = '';
+  
+        renderTheUI(); 
+      }
+  
 
-    newToDoText.value = '';
-    renderTheUI();
-  }
-
-  function renderTheUI() {
-   const toDoList = document.getElementById('toDoList');
-      
-   toDoList.textContent = '';
-
-   toDos.forEach(function(toDo) {
-     const newLi = document.createElement('li');
-     const checkbox = document.createElement('input');
-     checkbox.type = "checkbox";
-   
-     newLi.textContent = toDo.title;
-
-     toDoList.appendChild(newLi);
-     newLi.appendChild(checkbox);
-  });
-}
+      function renderTheUI(){
+        const toDoList = document.getElementById('toDoList');
+  
+        toDoList.textContent = '';
+  
+        toDos.forEach(function(toDo){
+          const newLi = document.createElement('li');
+          const checkbox = document.createElement('input');
+          checkbox.type = "checkbox";
+          const deleteBtn = document.createElement('button');
+          deleteBtn.textContent = "Delete";
+  
+          deleteBtn.addEventListener('click', event => {
+            toDos = toDos.filter(function(item){
+              return item.id !== toDo.id;
+            })
+            renderTheUI();
+          });
+  
+          newLi.textContent = toDo.title;
+  
+          toDoList.appendChild(newLi);
+          newLi.appendChild(checkbox);
+          newLi.appendChild(deleteBtn);
+        })
+      }
 
       addToDoForm.addEventListener('submit', event => {
       event.preventDefault();
